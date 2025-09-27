9cb0d66878292ac2b798d65270cb107a7f29fc0a
JurgenRauch <derserdani@gmail.com>
2025-09-27 13:32:36 +0200

    redirects

--- test-redirect.php
+++ test-redirect.php
@@ -10,7 +10,15 @@ $testUrls = [
     'index.php?f=bemutatkozas',
     'index.php?f=tevekenysegeink', 
     'index.php?f=elerhetoseg',
-    'index.php?f=arak'
+    'index.php?f=arak',
+    'index.php?f=activities',
+    'index.php?f=colleagues',
+    'index.php?f=references',
+    'index.php?f=munkatarsaink_benko_istvan',
+    'index.php?f=referenciak_kozbeszerzes',
+    'index.php?f=Envecon',
+    'index.php?f=letoltes_acs',
+    'index.php?f=Answer'
 ];
 
 echo "<h3>Test these old URLs:</h3>";
